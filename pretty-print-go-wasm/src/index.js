561141d6945ddf18b79051015873c09ce8d4c714
mandolyte <cecil.new@gmail.com>
2023-07-23 09:57:30 -0400

    trying ./main.wasm

--- pretty-print-go-wasm/src/index.js
+++ pretty-print-go-wasm/src/index.js
@@ -4,7 +4,7 @@ import Go from "./wasm_exec.js";
 
 const PrettyPrintGoWasm = async (jsonText) => {
     const go = new globalThis.Go();
-    const url = 'https://raw.githubusercontent.com/mandolyte/experiment-wasm/main/pretty-print-go-wasm/src/main.wasm'
+    const url = './main.wasm'
     await WebAssembly.instantiateStreaming(fetch(url), go.importObject).then((result) => {
         go.run(result.instance);
     });
